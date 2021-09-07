243b98e9b5022f27cbf50c955c64b0cb3362eade
Stephen <stephenszelpal@icloud.com>
2021-09-06 21:21:46 -0400

    getting there

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import './Dashboard.css';
+import me from './../../data/DashboardImage.png';
 
 function Dashboard() {
     return (
         <>
-            <h1>Dashboard</h1>
+            {/* looks like iMessage */}
+            <div className="dashboard-container">
+                <div className="dash-top">
+                    <div className="top-bumper"></div>
+                    <img src={me}></img>
+                    <span className="dash-name">Stephen Szelpal</span>
+                </div>
+
+                <div className="dash-mid"></div>
+                <div className="dash-bottom"></div>
+            </div>
         </>
     );
 }
