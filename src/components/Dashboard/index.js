3be3b3babedf5cac5f64e5a29ca1b4bbb5e6ffd8
Stephen <stephenszelpal@icloud.com>
2021-09-06 22:18:35 -0400

    text boxes in imessager done

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -8,13 +8,59 @@ function Dashboard() {
         <>
             {/* looks like iMessage */}
             <div className="dashboard-container">
+                {' '}
                 <div className="dash-top">
                     <div className="top-bumper"></div>
                     <img src={me}></img>
                     <span className="dash-name">Stephen Szelpal</span>
                 </div>
-
-                <div className="dash-mid"></div>
+                <div className="dash-mid">
+                    <div className="text">
+                        {/* row ONE of 'text' */}
+                        <div className="inner-text "></div>
+                        <div className="inner-text right">
+                            <li>Full Stack Software Engineer</li>
+                            <li>Full Stack Software Engineer</li>
+                        </div>
+                    </div>
+                    <div className="text">
+                        {/* row TWO of 'text'  */}
+                        <div className="inner-text left">
+                            <li>Full Stack Software Engineer</li>
+                            <li>Full Stack Software Engineer</li>
+                            <li>Full Stack Software Engineer</li>
+                            <li>Full Stack Software Engineer</li>
+                        </div>
+                        <div className="inner-text "></div>
+                    </div>
+                    <div className="text">
+                        {/* row THREE of 'text'  */}
+                        <div className="inner-text "></div>
+                        <div className="inner-text right">
+                            <li>Full Stack Software Engineer</li>
+                        </div>
+                    </div>
+                    <div className="text">
+                        {/* row FOUR of 'text'  */}
+                        <div className="inner-text left">
+                            <li>Full Stack Software Engineer</li>
+                            <li>Full Stack Software Engineer</li>
+                            <li>Full Stack Software Engineer</li>
+                        </div>
+                        <div className="inner-text "></div>
+                    </div>
+                    <div className="text">
+                        {/* row FIVE of 'text'  */}
+                        <div className="inner-text "></div>
+                        <div className="inner-text right">
+                            <li>Full Stack Software Engineer</li>
+                            <li>Full Stack Software Engineer</li>
+                            <li>Full Stack Software Engineer</li>
+                            <li>Full Stack Software Engineer</li>
+                            <li>Full Stack Software Engineer</li>
+                        </div>
+                    </div>
+                </div>
                 <div className="dash-bottom"></div>
             </div>
         </>
