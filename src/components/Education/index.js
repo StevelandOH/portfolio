36b908a10c4755f775ee0d0c0a8e2c7e1e807387
Stephen <stephenszelpal@icloud.com>
2021-09-14 08:38:29 -0400

    fixed education

--- src/components/Education/index.js
+++ src/components/Education/index.js
@@ -14,7 +14,7 @@ function Education() {
             <div className="edu csu">
                 <div className="edu app">
                     <div className="edu-text blue">
-                        'B.A. of Arts (unfinished)'
+                        Bachelor's in Arts (unfinished)
                     </div>
                     <div className="edu-text blue">
                         Cleveland State University
